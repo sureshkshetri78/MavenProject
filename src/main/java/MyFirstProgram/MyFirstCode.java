47b9776d36bab8589d618ba89335e537ad62e7c7
Suresh Basnet <sureshkshetri78@gmail.com>
2019-06-30 13:23:41 -0400

    First commit to maven

---
+++ src/main/java/MyFirstProgram/MyFirstCode.java
@@ -0,0 +1,27 @@
+package MyFirstProgram;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyFirstCode {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "D:\\QA Automation software\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("http://www.google.com");
+		driver.manage().deleteAllCookies();
+        driver.manage().window().maximize();
+        driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div/div[1]/div/div[1]/input")).sendKeys("selenium");
+        try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        driver.findElement(By.name("btnK")).click();
+        driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/div/div/div[1]/a/h3")).click();
+	}
+
+}
